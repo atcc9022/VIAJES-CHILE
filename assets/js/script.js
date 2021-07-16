1ceee40e79d54130b9e8bf120a55049c6ec78d57
alejandra <atcc9022@gmail.com>
2021-07-15 21:27:20 -0400

    cuarta version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 // // TYPEFORM
-(function () {
-    var qs, js, q, s, d = document,
-        gi = d.getElementById,
-        ce = d.createElement,
-        gt = d.getElementsByTagName,
-        id = "typef_orm",
-        b = "https://embed.typeform.com/";
-    if (!gi.call(d, id)) {
-        js = ce.call(d, "script");
-        js.id = id;
-        js.src = b + "embed.js";
-        q = gt.call(d, "script")[0];
-        q.parentNode.insertBefore(js, q)
-    }
-})()
+// (function () {
+//     var qs, js, q, s, d = document,
+//         gi = d.getElementById,
+//         ce = d.createElement,
+//         gt = d.getElementsByTagName,
+//         id = "typef_orm",
+//         b = "https://embed.typeform.com/";
+//     if (!gi.call(d, id)) {
+//         js = ce.call(d, "script");
+//         js.id = id;
+//         js.src = b + "embed.js";
+//         q = gt.call(d, "script")[0];
+//         q.parentNode.insertBefore(js, q)
+//     }
+// })()
 
-// TOOLTIP(para iconos de "quienes somos")
-$(document).ready(function(){
-    $('fas fa-plane').tooltip({title:"AVION", trigger: "hover"});
-    $('fas fa-mountain').tooltip({title:"MONTAÑA", trigger: "hover"});
-    $('fas fa-route').tooltip({title:"RUTA", trigger: "hover"});
+// // TOOLTIP(para iconos de "quienes somos")
+// $(document).ready(function(){
+//     $('fas fa-plane').tooltip({title:"AVION", trigger: "hover"});
+//     $('fas fa-mountain').tooltip({title:"MONTAÑA", trigger: "hover"});
+//     $('fas fa-route').tooltip({title:"RUTA", trigger: "hover"});
 
 
-});
+// });
 
